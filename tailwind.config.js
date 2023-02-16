7a8ed5cdd09ed49d36b7b20a2e72cb70e35080be
riccardo.linares <linares.riccardo@gmail.com>
2023-02-16 19:33:45 +0100

    projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: {
+          50: "#fdf8f6",
+          100: "#f2e8e5",
+          200: "#eaddd7",
+          300: "#e0cec7",
+          400: "#d2bab0",
+          500: "#bfa094",
+          600: "#a18072",
+          700: "#977669",
+          800: "#846358",
+          900: "#43302b",
+        },
+        primary: "#111827",
+        secondary: "#6b7280",
+        accent: "#fef08a",
+      },
+    },
   },
   plugins: [],
 };
